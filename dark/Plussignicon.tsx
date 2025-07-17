6c0f5e434fc336cf83f03b7bf4c8bb57f9da1c39
Abderrahman Arid <arid07abderrahman@gmail.com>
2025-07-18 00:50:28 +0200

    Add SVG icons for various functionalities including Checkmark, Documents, Edit, and more
    - Implement Checkmarkicon component
    - Implement Documents component
    - Implement Edit component
    - Implement Editaddtoqueue component
    - Implement Edithide component
    - Implement Exitright component
    - Implement Headphones component
    - Implement Insights component
    - Implement Marketing component
    - Implement Notifications component (renamed to TruckFast)
    - Implement Overview component
    - Implement Paperclip component
    - Implement Plussignicon component
    - Implement Settings component

---
+++ dark/Plussignicon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+ const Plussignicon = (props: React.SVGProps<SVGSVGElement>) => {
+    return (
+       <svg {...props }
+ width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_158_997)">
+<path d="M10.7299 17.5V6.7H13.2979V17.5H10.7299ZM6.48188 13.3V10.9H17.5459V13.3H6.48188Z" fill="white"/>
+</g>
+<defs>
+<clipPath id="clip0_158_997">
+<rect width="24" height="24" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+
+    );
+};
+
+export default Plussignicon;

e20246c83ac0156e78888afc54f752a90d387798
Abderrahman Arid <arid07abderrahman@gmail.com>
2025-07-18 14:16:40 +0200

    Add SVG components for ArrowClipart, Basket, Edit2, and Marketing2
    - Implemented ArrowClipart component with a white arrow design.
    - Created Basket component featuring a detailed basket icon.
    - Developed Edit2 component representing an edit icon with a pencil.
    - Added Marketing2 component showcasing a marketing-related graphic.

---
+++ dark/ArrowClipart.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+ const ArrowClipart = (props: React.SVGProps<SVGSVGElement>) => {
+    return (
+        <svg {...props }
+ width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M13.2 10.9043L13.2 5.42607H16.8L12 1.04346L7.2 5.42607H10.8L10.8 10.9043L4.8 10.9043V7.61737L0 12L4.8 16.3826V13.0956L10.8 13.0956V18.5739H7.2L12 22.9565L16.8 18.5739L13.2 18.5739V13.0956H19.2L19.2 16.3826L24 12L19.2 7.61737V10.9043H13.2Z" fill="white"/>
+</svg>
+
+    );
+};
+
+export default ArrowClipart;
+

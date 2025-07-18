56dc90b67678f87d749d0d0f15747bdf76d7f3fa
Abderrahman Arid <arid07abderrahman@gmail.com>
2025-07-18 15:22:22 +0200

    Add SVG components for DecisionTree, InfoCirclee, Play, and Wirechat with detailed path definitions

---
+++ white/InfoCirclee.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+ const InfoCirclee = (props: React.SVGProps<SVGSVGElement>) => {
+    return (
+        <svg {...props }
+ width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.9993 20.3332C7.39698 20.3332 3.66602 16.6022 3.66602 11.9998C3.66602 7.39746 7.39698 3.6665 11.9993 3.6665C16.6017 3.6665 20.3327 7.39746 20.3327 11.9998C20.3276 16.6001 16.5996 20.3281 11.9993 20.3332ZM10.3243 11.1582V12.8332L11.1577 12.8332L11.1577 16.1665H13.6743V14.4998H12.8327L12.841 11.159L10.3243 11.1582ZM11.1577 7.83317V9.51567H12.841V7.83317H11.1577Z" fill="black"/>
+</svg>
+
+    );
+};
+
+export default InfoCirclee;
+
+
+
+
---
+++ white/Wirechat.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+ const Wirechat = (props: React.SVGProps<SVGSVGElement>) => {
+    return (
+       <svg {...props }
+ width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_i_158_11427)">
+<rect x="1" y="1" width="22" height="22" rx="5.5" fill="url(#paint0_linear_158_11427)"/>
+<rect x="1.34375" y="1.34375" width="21.3125" height="21.3125" rx="5.15625" stroke="black" stroke-width="0.6875"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M7.46627 13.4212C7.54197 13.5699 7.69469 13.6635 7.86152 13.6635H10.9664V17.3597C10.9664 17.5518 11.09 17.7221 11.2727 17.7815C11.4554 17.8409 11.6556 17.776 11.7686 17.6206L16.4998 11.1152C16.5979 10.9803 16.612 10.8017 16.5363 10.6531C16.4606 10.5044 16.3079 10.4108 16.1411 10.4108H13.0362V6.71457C13.0362 6.52246 12.9126 6.35218 12.7299 6.29277C12.5472 6.23336 12.347 6.29832 12.234 6.45369L7.50281 12.9591C7.40468 13.094 7.39056 13.2726 7.46627 13.4212ZM12.1492 8.07857V10.8544C12.1492 11.0993 12.3477 11.2979 12.5927 11.2979H15.2701L11.8535 15.9957V13.2199C11.8535 12.975 11.6549 12.7764 11.4099 12.7764H8.73255L12.1492 8.07857Z" fill="white"/>
+</g>
+<defs>
+<filter id="filter0_i_158_11427" x="1" y="1" width="22" height="22" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset/>
+<feGaussianBlur stdDeviation="1.375"/>
+<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0.745098 0 0 0 0 0.866667 0 0 0 0 0.992157 0 0 0 1 0"/>
+<feBlend mode="normal" in2="shape" result="effect1_innerShadow_158_11427"/>
+</filter>
+<linearGradient id="paint0_linear_158_11427" x1="12" y1="1" x2="12" y2="23" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-color="#2D2D2D"/>
+</linearGradient>
+</defs>
+</svg>
+
+    );
+};
+
+export default Wirechat;
+
+
+
+

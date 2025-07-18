cf9a62b4a3df1f636f022f2b3da1cbb769307276
Abderrahman Arid <arid07abderrahman@gmail.com>
2025-07-18 13:34:34 +0200

    Add SVG icons for InfoCirclee and MinusCircle components

---
+++ dark/MinusCircle.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+  const MinusCircle = (props: React.SVGProps<SVGSVGElement>) => {
+    return (
+        <svg {...props }
+ width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.0013 20.3332C7.40102 20.3281 3.67302 16.6001 3.66797 11.9999V11.8332C3.75958 7.25363 7.53012 3.60648 12.1102 3.66725C16.6903 3.72802 20.3627 7.47392 20.3328 12.0543C20.3029 16.6347 16.5818 20.3323 12.0013 20.3332ZM7.83464 11.1665V12.8332L16.168 12.8332V11.1665L7.83464 11.1665Z" fill="white"/>
+</svg>
+
+    );
+};
+
+export default MinusCircle;
+
